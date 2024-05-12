125cda13ad1c6a837626e391ddae21bffd743658
mashfei-laptop <evil-corporaty@yandex.ru>
2024-05-12 03:07:18 +0300

    Add styling for sections, created general classes for fonts

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,4 +1,8 @@
-// Подключение функционала "Чертогов Фрилансера"
-// import { isMobile } from "./functions.js";
-// Подключение списка активных модулей
-// import { flsModules } from "./modules.js";
+$(document).ready(function () {
+  $(".slider__slider").slick({
+    prevArrow: ".slider__button_prev",
+    nextArrow: ".slider__button_next",
+    speed: 800,
+    lazyLoad: "progressive",
+  });
+});
