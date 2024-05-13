ca5d6e40048de81a3e4305f487574964d96e9aa5
mashfei-laptop <evil-corporaty@yandex.ru>
2024-05-13 14:33:05 +0300

    Webp support removed

--- config/gulp-tasks/html.js
+++ config/gulp-tasks/html.js
@@ -1,48 +1,50 @@
-import htmlmin from 'gulp-htmlmin';
-import typograf from 'gulp-typograf';
-import versionNumber from 'gulp-version-number';
-import webpHtmlNosvg from 'gulp-webp-html-nosvg';
+import htmlmin from "gulp-htmlmin";
+import typograf from "gulp-typograf";
+import versionNumber from "gulp-version-number";
+import webpHtmlNosvg from "gulp-webp-html-nosvg";
 
 export const html = () => {
-  return app.gulp
-    .src(`${app.path.build.html}*.html`)
-    .pipe(
-      app.plugins.plumber(
-        app.plugins.notify.onError({
-          title: 'HTML',
-          message: 'Error: <%= error.message %>',
+  return (
+    app.gulp
+      .src(`${app.path.build.html}*.html`)
+      .pipe(
+        app.plugins.plumber(
+          app.plugins.notify.onError({
+            title: "HTML",
+            message: "Error: <%= error.message %>",
+          })
+        )
+      )
+      .pipe(
+        typograf({
+          locale: ["ru", "en-US"],
+          htmlEntity: "name",
         })
       )
-    )
-    .pipe(
-      typograf({
-        locale: ['ru', 'en-US'],
-        htmlEntity: 'name',
-      })
-    )
-    .pipe(webpHtmlNosvg())
-    .pipe(
-      versionNumber({
-        value: '%DT%',
-        append: {
-          key: '_v',
-          cover: 0,
-          to: ['css', 'js', 'img'],
-        },
-        output: {
-          file: 'config/version.json',
-        },
-      })
-    )
-    .pipe(
-      app.plugins.if(
-        app.isBuild,
-        htmlmin({
-          removeComments: true,
-          collapseWhitespace: true,
-          minifyCSS: true,
+      // .pipe(webpHtmlNosvg())
+      .pipe(
+        versionNumber({
+          value: "%DT%",
+          append: {
+            key: "_v",
+            cover: 0,
+            to: ["css", "js", "img"],
+          },
+          output: {
+            file: "config/version.json",
+          },
         })
       )
-    )
-    .pipe(app.gulp.dest(app.path.build.html));
+      // .pipe(
+      //   app.plugins.if(
+      //     app.isBuild,
+      //     htmlmin({
+      //       removeComments: true,
+      //       collapseWhitespace: true,
+      //       minifyCSS: true,
+      //     })
+      //   )
+      // )
+      .pipe(app.gulp.dest(app.path.build.html))
+  );
 };
--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,8 +1,35 @@
 $(document).ready(function () {
-  $(".slider__slider").slick({
-    prevArrow: ".slider__button_prev",
-    nextArrow: ".slider__button_next",
-    speed: 800,
-    lazyLoad: "progressive",
+  $(".slider__slider").each(function (slider) {
+    $(this).slick({
+      prevArrow: this.closest(".slider").querySelector(".slider__button_prev"),
+      nextArrow: this.closest(".slider").querySelector(".slider__button_next"),
+      speed: 800,
+      lazyLoad: "progressive",
+    });
+  });
+  $(".overflow-slider").each(function (slider) {
+    $(this)
+      .find(".overflow-slider__slider")
+      .slick({
+        prevArrow: this.closest(".overflow-slider").querySelector(".slider__button_prev"),
+        nextArrow: this.closest(".overflow-slider").querySelector(".slider__button_next"),
+        speed: 800,
+        lazyLoad: "progressive",
+      });
+  });
+  $(".linked-slider").each(function (slider) {
+    $(this)
+      .find(".linked-slider__text-slider")
+      .slick({
+        prevArrow: this.querySelector(".slider__button_prev"),
+        nextArrow: this.querySelector(".slider__button_next"),
+        speed: 800,
+        asNavFor: $(this).find(".linked-slider__slider-block"),
+      });
+    $(this).find(".linked-slider__slider-block").slick({
+      speed: 800,
+      fade: true,
+      arrows: false,
+    });
   });
 });
