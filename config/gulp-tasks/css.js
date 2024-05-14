df6059747029f0197a62c956c6818891980d3fe1
lay_morja <evil-corporaty@yandex.ru>
2024-05-14 18:01:12 +0300

    Remove webp transformation for styles

--- config/gulp-tasks/css.js
+++ config/gulp-tasks/css.js
@@ -4,42 +4,28 @@ import groupCssMediaQueries from "gulp-group-css-media-queries";
 import webpcss from "gulp-webpcss";
 
 export const css = () => {
-  return (
-    app.gulp
-      .src(`${app.path.build.css}/style.css`, {})
-      .pipe(
-        app.plugins.plumber(
-          app.plugins.notify.onError({
-            title: "CSS",
-            message: "Error: <%= error.message %>",
-          })
-        )
+  return app.gulp
+    .src(`${app.path.build.css}/style.css`, {})
+    .pipe(
+      app.plugins.plumber(
+        app.plugins.notify.onError({
+          title: "CSS",
+          message: "Error: <%= error.message %>",
+        })
       )
-      .pipe(app.plugins.if(app.isBuild, groupCssMediaQueries()))
-      .pipe(
-        app.plugins.if(
-          app.isBuild,
-          autoprefixer({
-            grid: true,
-            overrideBrowserslist: ["last 3 versions"],
-            cascade: false,
-          })
-        )
+    )
+    .pipe(app.plugins.if(app.isBuild, groupCssMediaQueries()))
+    .pipe(
+      app.plugins.if(
+        app.isBuild,
+        autoprefixer({
+          grid: true,
+          overrideBrowserslist: ["last 3 versions"],
+          cascade: false,
+        })
       )
-      // .pipe(
-      //   app.plugins.if(
-      //     app.isWebP,
-      //     app.plugins.if(
-      //       app.isBuild,
-      //       webpcss({
-      //         webpClass: '.webp',
-      //         noWebpClass: '.no-webp',
-      //       })
-      //     )
-      //   )
-      // )
-      .pipe(app.plugins.if(app.isBuild, cleanCss()))
-      .pipe(app.plugins.rename({ suffix: ".min" }))
-      .pipe(app.gulp.dest(app.path.build.css))
-  );
+    )
+    .pipe(app.plugins.if(app.isBuild, cleanCss()))
+    .pipe(app.plugins.rename({ suffix: ".min" }))
+    .pipe(app.gulp.dest(app.path.build.css));
 };
